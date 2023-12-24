e236572eb8a87f6c4670d8c7fa89506a28ebc6b1
Diego Fernandes <diego.silva@luz-ef.com>
2023-12-24 19:06:08 -0400

    compilando

--- app/src/main/java/com/dg/funami/MainActivity.kt
+++ app/src/main/java/com/dg/funami/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.dg.funami
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,37 +12,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.drawerlayout.widget.DrawerLayout
 import com.dg.funami.ui.theme.FunamiTheme
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FunamiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.main)
+        // Define os itens do menu
+        val menu = NavigationView(this).apply {
+            inflateMenu(R.menu.menu_main)
         }
+
+        Log.d("zzz", "vai começar")
+
+        // Adiciona um ouvinte para eventos de clique nos itens do menu
+        menu.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_item_1 -> clicouDinossauro(item.itemId)
+                R.id.nav_item_2 -> clicouDinossauro(item.itemId)
+            }
+            true
+        }
+
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FunamiTheme {
-        Greeting("Android")
+    fun clicouDinossauro(id: Int) {
+        Log.d("zzz", "clicou $id")
     }
 }
