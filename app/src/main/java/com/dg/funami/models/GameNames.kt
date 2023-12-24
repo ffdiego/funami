62c03aec302205e65c4e5e82a78a2fb80a28b3ea
Diego Fernandes <diego.silva@luz-ef.com>
2023-12-24 15:33:05 -0400

    adiciona primeira classe em kotlin

---
+++ app/src/main/java/com/dg/funami/models/GameNames.kt
@@ -0,0 +1,8 @@
+package com.dg.funami.models
+
+enum class GameNames {
+    GTA2,
+    GTA_VICE_CITY,
+    GTA_SAN_ANDREAS,
+    GTA5
+}
---
+++ app/src/main/java/com/dg/funami/models/RadioStation.kt
@@ -0,0 +1,7 @@
+package com.dg.funami.models
+
+class RadioStation(val game: GameNames) {
+    var name: String? = null
+    var url: String? = null
+    var image: String? = null
+}
