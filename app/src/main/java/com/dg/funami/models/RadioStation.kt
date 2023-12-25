e3bdc88ad882123ea393e5b3b2894f77ddf2e9d2
Diego Fernandes <diego.silva@luz-ef.com>
2023-12-25 02:45:29 -0400

    play.xml e lógica de radiostation

--- app/src/main/java/com/dg/funami/models/RadioStation.kt
+++ app/src/main/java/com/dg/funami/models/RadioStation.kt
@@ -1,7 +1,16 @@
 package com.dg.funami.models
 
-class RadioStation(val game: GameNames) {
+import android.media.MediaPlayer
+
+class RadioStation(val mediaPlayer: MediaPlayer, val game: GameNames) {
     var name: String? = null
     var url: String? = null
     var image: String? = null
+    val path: String = ""
+
+    fun play() {
+        this.mediaPlayer.setDataSource(this.path)
+        this.mediaPlayer.seekTo(this.mediaPlayer.duration / 2)
+        this.mediaPlayer.start()
+    }
 }
